db614bfaf6e31b226dd372c47e90f31164501fca
charfisirine <charfisirine62@gmail.com>
2023-12-18 19:09:09 +0100

    Premier commit avec les fichiers du projet

---
+++ app/Http/Controllers/PanierController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Panier;
+use Illuminate\Http\Request;
+
+class PanierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Panier $panier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Panier $panier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Panier $panier)
+    {
+        //
+    }
+}
