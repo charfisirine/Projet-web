3bf6f73fe6ccbd0eb525eb85b4799745c5dc8859
charfisirine <charfisirine62@gmail.com>
2023-12-24 09:36:06 +0100

    template acceuil

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from "vue-router";
 import { routes } from "./routes";
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.vue'
 let app = createApp(App)
 
--- resources/js/components/accueil.vue
+++ resources/js/components/accueil.vue
@@ -1,22 +1,515 @@
 <template>
-    <div class="container">
-    <div class="row justify-content-center">
-    <div class="col-md-8">
-    <div class="card">
-    <div class="card-header">Bienvenue dans notre site</div>
-    <div class="card-body">
-    Vous êtes à la page d'accueil
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-</template>
 
+<br>
+	<!-- banner bottom   -->
+	<div class="banner-bottom py-5">
+		<div class="d-md-flex container py-xl-3 py-lg-3">
+			<div class="banner-left-bottom-w3ls">
+				<h6 class="text-wh let">Sed do eiusmod tempor</h6>
+				<h3 class="text-wh my-3">Welcome to our Exceptionel Agence </h3>
+				<p>Enjoy Your Every Moments With Your Partner In Villas  Resort.<br>Sed do eiusmod tempor incididunt utlabore.</p>
+			</div>
+			<div class="button offset-lg-2 offset-md-1">
+				<a href="about.html" class="btn w3ls-button-mobamu">Read More</a>
+			</div>
+		</div>
+	</div>
+	<!-- //banner bottom -->
+    <br>
+<hr>
+<br>
+	<!-- about bech n7otha  -->
+	<section class="about py-5" id="about">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row py-md-5">
+				<div class="col-lg-4 about-left-w3pvt offset-lg-1 mt-lg-4">
+					<div class="main-img">
+						<img src="../../assets/images/log1.jpg" alt="" class="img-fluid pos-aboimg">
+                        <img src="../../assets/images/log2.jpg" alt="" class="img-fluid pos-aboimg2">
+
+                    </div>
+				</div>
+				<div class="col-xl-6 col-lg-7 about-right offset-xl-1">
+                    <h4 style="font-size: 60px; color: red;">About Us</h4>
+					<h3 class="tittle-w3layouts text-uppercase pr-lg-5 mt-2">Duis aute irure olor in cillum solore</h3>
+					<p class="mt-4 mb-4">Lorem ipsum dolor sit amet Neque porro quisquam est qui dolorem Lorem int ipsum dolor sit
+						amet id tempor felis. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit.</p>
+					<a href="about.html" class="btn button-style-2 mt-sm-5 mt-4">Read More</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //about -->
+<hr>
+<br>
+
+
+	<!-- stats -->
+	<div class="stats py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row stats-mobamu py-5">
+				<div class="col-lg-4 col-md-6">
+					<div class="stats-grid">
+						<div class="row">
+							<div class="col-4 text-center">
+								<span class="fa fa-star"></span>
+							</div>
+							<div class="col-8">
+								<h4 class="numscroller">6000</h4>
+								<p>Excellent Ratings</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 mt-md-0 mt-4">
+					<div class="stats-grid">
+						<div class="row">
+							<div class="col-4 text-center">
+								<span class="fa fa-smile-o"></span>
+							</div>
+							<div class="col-8">
+								<h4 class="numscroller">25k</h4>
+								<p>Happy Guests</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 mx-lg-0 mx-md-auto mt-lg-0 mt-4">
+					<div class="stats-grid">
+						<div class="row">
+							<div class="col-4 text-center">
+								<span class="fa fa-cutlery"></span>
+							</div>
+							<div class="col-8">
+								<h4 class="numscroller">150</h4>
+								<p>Food Items</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //stats -->
+    <br>
+
+    	<!-- price -->
+	<div class="rooms-w3ls bg-li py-5" id="price">
+		<div class="container-fluid py-xl-5 py-lg-3">
+			<h3 class="tittle text-center text-bl font-weight-bold">Rooms & Suites</h3>
+			<p class="sub-tittle text-center mt-2 mb-sm-5 mb-4 pb-xl-3">Sed do eiusmod tempor incididunt ut labore et dolore
+				magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+			<div class="row">
+				<div class="col-lg-4 price-mobamus">
+					<div class="price-top">
+						<a href="single.html">
+							<img src="../../assets/images/bedroom1.jpg" alt="" class="img-fluid" />
+						</a>
+					</div>
+					<div class="price-w3ls-bottom p-4">
+						<h4 class="my-2"><a href="single.html">Suite</a></h4>
+						<div class="lm-item-price">
+							<h6>
+								<span class="price-top-head">$</span>
+								<span class="price-midd-head">195</span>
+								<span class="price-right-head">/ per month</span>
+							</h6>
+						</div>
+						<ul class="style-lists">
+							<li>Only 5 rooms are available </li>
+							<li>Breakfast included </li>
+							<li>Price does not include VAT & services fee </li>
+						</ul>
+						<a href="book.html" class="btn button-style-2 mt-sm-5 mt-4">Book Now</a>
+					</div>
+				</div>
+				<div class="col-lg-4 price-mobamus my-lg-0 my-5">
+					<div class="price-top">
+						<a href="single.html">
+							<img src="../../assets/images/bedroom2.jpg" alt="" class="img-fluid" />
+						</a>
+					</div>
+					<div class="price-w3ls-bottom p-4">
+						<h4 class="my-2"><a href="single.html">Deluxe Room</a></h4>
+						<div class="lm-item-price">
+							<h6>
+								<span class="price-top-head">$</span>
+								<span class="price-midd-head">120</span>
+								<span class="price-right-head">/ per month</span>
+							</h6>
+						</div>
+						<ul class="style-lists">
+							<li>Perfect for traveling couples </li>
+							<li>Breakfast included </li>
+							<li>Concierge services </li>
+						</ul>
+						<a href="book.html" class="btn button-style-2 mt-sm-5 mt-4">Book Now</a>
+					</div>
+				</div>
+				<div class="col-lg-4 price-mobamus">
+					<div class="price-top">
+						<a href="single.html">
+							<img src="../../assets/images/bedroom3.jpg" alt="" class="img-fluid" />
+						</a>
+					</div>
+					<div class="price-w3ls-bottom p-4">
+						<h4 class="my-2"><a href="single.html">Family Room</a></h4>
+						<div class="lm-item-price">
+							<h6>
+								<span class="price-top-head">$</span>
+								<span class="price-midd-head">182</span>
+								<span class="price-right-head">/ per month</span>
+							</h6>
+						</div>
+						<ul class="style-lists">
+							<li>Two double beds </li>
+							<li>Babysitting facilities</li>
+							<li>1 free bed available on request</li>
+						</ul>
+						<a href="book.html" class="btn button-style-2 mt-sm-5 mt-4">Book Now</a>
+                        <!--<a href="book.html" class="btn button-style-2 mt-sm-5 mt-4">Book Now</a>-->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //price -->
+    <br>
+
+	<!-- blog -->
+	<div class="blog-w3ls py-5" id="blog">
+		<div class="container py-xl-5 py-lg-3">
+			<h3 class="tittle text-center text-bl font-weight-bold">Accommodation</h3>
+			<p class="sub-tittle text-center mt-2 mb-sm-5 mb-4 pb-xl-3">Sed do eiusmod tempor incididunt ut labore et dolore
+				magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+			<div class="row package-grids">
+				<div class="col-lg-4 posts-mobamus">
+					<div class="posts-top">
+						<a href="single.html">
+							<img src="../../assets/images/hotel1.jfif" alt="" class="img-fluid" />
+						</a>
+						<h3>13
+							<span>Sep</span>
+						</h3>
+					</div>
+					<div class="posts-w3ls-bottom py-4">
+						<h4 class="mb-2"><a href="single.html">Sit accus antium</a></h4>
+						<ul class="list-unstyled">
+							<li>
+								<a href="#">
+									<span class="fa fa-thumbs-up mr-1"></span>20 Likes</a>
+							</li>
+							<li class="mx-3">
+								<a href="#">
+									<span class="fa fa-comments mr-1"></span>48 Comments</a>
+							</li>
+						</ul>
+						<p class="mt-3">sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 posts-mobamus my-lg-0 my-4">
+					<div class="posts-top">
+						<a href="single.html">
+							<img src="../../assets/images/hotel1.jfif" alt="" class="img-fluid" />
+						</a>
+						<h3>16
+							<span>Sep</span>
+						</h3>
+					</div>
+					<div class="posts-w3ls-bottom py-4">
+						<h4 class="mb-2"><a href="single.html">Sit accus antium</a></h4>
+						<ul class="list-unstyled">
+							<li>
+								<a href="#">
+									<span class="fa fa-thumbs-up mr-1"></span>20 Likes</a>
+							</li>
+							<li class="mx-3">
+								<a href="#">
+									<span class="fa fa-comments mr-1"></span>48 Comments</a>
+							</li>
+						</ul>
+						<p class="mt-3">sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 posts-mobamus">
+					<div class="posts-top">
+						<a href="single.html">
+							<img src="../../assets/images/hotel1.jfif" alt="" class="img-fluid" />
+						</a>
+						<h3>20
+							<span>Sep</span>
+						</h3>
+					</div>
+					<div class="posts-w3ls-bottom py-4">
+						<h4 class="mb-2"><a href="single.html">Sit accus antium</a></h4>
+						<ul class="list-unstyled">
+							<li>
+								<a href="#">
+									<span class="fa fa-thumbs-up mr-1"></span>20 Likes</a>
+							</li>
+							<li class="mx-3">
+								<a href="#">
+									<span class="fa fa-comments mr-1"></span>48 Comments</a>
+							</li>
+						</ul>
+						<p class="mt-3">sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //blog -->
+
+	<!-- services -->
+	<div class="serives-mobamu py-5" id="services">
+		<div class="container py-xl-5 py-lg-3">
+			<h3 class="tittle text-center text-wh font-weight-bold">Our Services</h3>
+			<p class="sub-tittle text-center text-li mt-2 mb-sm-5 mb-4 pb-xl-3">Sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+			<div class="row welcome-bottom text-center">
+				<div class="col-lg-3 col-sm-6 px-2">
+					<div class="welcome-grid bg-wh py-5 px-4">
+						<img src="../../assets/images/hotel1.jfif" alt="" class="img-fluid">
+						<h4 class="mt-4 mb-3 text-bl">Service 01</h4>
+						<p>Quis nostrum ullam corporis suscipit.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6 mt-sm-0 mt-5 px-2">
+					<div class="welcome-grid bg-wh py-5 px-4">
+						<img src="../../assets/images/hotel1.jfif" alt="" class="img-fluid">
+						<h4 class="mt-4 mb-3 text-bl">Service 02</h4>
+						<p>Quis nostrum ullam corporis suscipit.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6 mt-lg-0 mt-5 px-2">
+					<div class="welcome-grid bg-wh py-5 px-4">
+						<img src="../../assets/images/hotel1.jfif" alt="" class="img-fluid">
+						<h4 class="mt-4 mb-3 text-bl">Service 03</h4>
+						<p>Quis nostrum ullam corporis suscipit.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6 mt-lg-0 mt-5 px-2">
+					<div class="welcome-grid bg-wh py-5 px-4">
+						<img src="../../assets/images/hotel1.jfif" alt="" class="img-fluid">
+						<h4 class="mt-4 mb-3 text-bl">Service 04</h4>
+						<p>Quis nostrum ullam corporis suscipit.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //services -->
+
+	<!-- testimonials -->
+	<section class="team-main-sec bg-li py-5" id="testi">
+		<div class="container py-xl-5 py-lg-3">
+			<h3 class="tittle text-center text-bl font-weight-bold">Our Guests Love Us</h3>
+			<p class="sub-tittle text-center mt-2 mb-sm-5 mb-4 pb-xl-3">Sed do eiusmod tempor incididunt ut labore et dolore
+				magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+			<div class="row text-center">
+				<div class="col-lg-4 col-md-6 team-gd-info">
+					<div class="team-gd">
+						<div class="team-img mb-4">
+							<img src="../../assets/images/hotel1.jfif" class="img-fluid" alt="user-image">
+						</div>
+						<div class="team-info">
+							<h3>Jason Dono <span class="sub-tittle-team">New York</span> </h3>
+							<ul class="list-unstyled my-md-4 my-3">
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star-o"></span></li>
+							</ul>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 team-gd-info mt-md-0 mt-5">
+					<div class="team-gd">
+						<div class="team-img mb-4">
+							<img src="../../assets/images/hotel1.jfif" class="img-fluid" alt="user-image">
+						</div>
+						<div class="team-info">
+							<h3>Mariana Noe <span class="sub-tittle-team">Italy</span></h3>
+							<ul class="list-unstyled my-md-4 my-3">
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star-half-o"></span></li>
+							</ul>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 mx-lg-0 mx-md-auto team-gd-info mt-lg-0 mt-5">
+					<div class="team-gd">
+						<div class="team-img mb-4">
+							<img src="../../assets/images/hotel1.jfif" class="img-fluid" alt="user-image">
+						</div>
+						<div class="team-info">
+							<h3>Daniel Doe <span class="sub-tittle-team">Germany</span></h3>
+							<ul class="list-unstyled my-md-4 my-3">
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+								<li><span class="fa fa-star"></span></li>
+							</ul>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //testimonials -->
+
+	<!-- middle -->
+	<div class="middle py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="welcome-left text-center py-md-5 py-3">
+				<h3>Enjoy Your Every Moments With Your Partner In Our Hotels Resort</h3>
+				<a href="book.html" class="btn button-style-2  mt-sm-5 mt-4">Book Now</a>
+			</div>
+		</div>
+	</div>
+	<!-- //middle -->
+
+	<!-- newsletter -->
+	<div class="newsletter_w3w3pvt py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<h3 class="tittle text-center text-bl font-weight-bold">Subscribe Newsletter</h3>
+			<p class="sub-tittle text-center mt-2 mb-sm-5 mb-4 pb-xl-3">Sed do eiusmod tempor incididunt ut labore et dolore
+				magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+			<div class="newsletter">
+				<form action="#" method="post">
+					<div class="form-group mb-0 w-100">
+						<input class="email" type="email" name="email" placeholder="Your email..." required="">
+					</div>
+					<button type="submit" class="btn">Subscribe</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- //newsletter -->
+
+	<!-- footer -->
+	<footer class="py-5">
+		<div class="container pt-xl-4">
+			<div class="row footer-top">
+				<div class="col-lg-4 col-md-6 footer-grid_section_1its">
+					<h2 class="logo-2 mb-lg-4 mb-3">
+						<a href="index.html" class="text-uppercase text-wh">Villas</a>
+					</h2>
+					<!-- Map -->
+					<div class="map-fo">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3048.5130900428026!2d44.506621715459396!3d40.17539347939393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abcf86e940951%3A0x87a1923d7df42367!2sBest+Western+Congress+Hotel!5e0!3m2!1sen!2sin!4v1509605601510"></iframe>
+					</div>
+					<!-- //Map -->
+				</div>
+				<div class="col-lg-2 col-md-6 footer-grid_section_1its mt-md-0 mt-4">
+					<h3 class="footer-title text-uppercase text-wh mb-lg-4 mb-3">Links</h3>
+					<ul class="list-unstyled">
+						<li class="mb-3">
+							<a href="index.html">Home</a>
+						</li>
+						<li class="mb-3">
+							<a href="about.html">About Us</a>
+						</li>
+						<li class="mb-3">
+							<a href="gallery.html">Gallery</a>
+						</li>
+						<li class="mb-3">
+							<a href="#services">Services</a>
+						</li>
+						<li class="mb-3">
+							<a href="contact.html">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-grid_section_1its mt-lg-0 mt-4">
+					<h3 class="footer-title text-uppercase text-wh mb-lg-4 mb-3">Contact Info</h3>
+					<div class="contact-info">
+						<div class="footer-style-w3ls mb-4">
+							<h4 class="text-li mb-2">Phone</h4>
+							<p>+121 098 8907 9987</p>
+						</div>
+						<div class="footer-style-w3ls mb-4">
+							<h4 class="text-li mb-2">Email </h4>
+							<p><a href="mailto:info@example.com">info@example.com</a></p>
+						</div>
+						<div class="footer-style-w3ls mb-4">
+							<h4 class="text-li mb-2">Location</h4>
+							<p>Honey Avenue, New York City</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-grid_section_1its mt-lg-0 mt-4">
+					<!-- social icons -->
+					<div class="mobamuinfo_social_icons">
+						<h3 class="footer-title text-uppercase text-wh mb-lg-4 mb-3">Social Info</h3>
+						<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<h4 class="sub-con-fo text-li my-4">Catch on Social</h4>
+						<ul class="mobamuits_social_list list-unstyled">
+							<li class="w3_mobamu_facebook">
+								<a href="#">
+									<span class="fa fa-facebook-f"></span>
+								</a>
+							</li>
+							<li class="w3_mobamu_twitter">
+								<a href="#">
+									<span class="fa fa-twitter"></span>
+								</a>
+							</li>
+							<li class="w3_mobamu_dribble">
+								<a href="#">
+									<span class="fa fa-dribbble"></span>
+								</a>
+							</li>
+							<li class="w3_mobamu_google">
+								<a href="#">
+									<span class="fa fa-google-plus"></span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<!-- social icons -->
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="cpy-right text-center py-3">
+		<p>© 2023 All rights reserved | Design by wiem & sirine</p>
+	</div>
+
+</template>
 <script>
-    export default {
-    mounted() {
-    console.log('Component mounted.')
-    }
-    }
-</script>
+
+	export default {
+	mounted() {
+	console.log('Component mounted.')
+	}
+	}
+
+	addEventListener("load", function () {
+	setTimeout(hideURLbar, 0);
+	}, false);
+
+	function hideURLbar() {
+	window.scrollTo(0, 1);
+	}
+	</script>
+	<style>
+	@import './style.css';
+	</style>
