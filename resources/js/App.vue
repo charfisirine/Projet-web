4a58584c97c12cb8579554a2ef41a71de70b90be
charfisirine <charfisirine62@gmail.com>
2024-01-17 14:23:01 +0100

    Acceuil partie hotems final2

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,51 +1,47 @@
-
 <template>
     <div>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="collapse navbar-collapse">
           <div class="navbar-nav">
             <router-link to="/" class="nav-item nav-link">Accueil</router-link>
-            <router-link to="/Addreservation" class="nav-item nav-link">Reservation</router-link>
+            <router-link to="/Addreservation" class="nav-item nav-link"> Reservation</router-link>
             <router-link to="/dashboard" class="nav-item nav-link">Dashboard</router-link>
             <router-link to="/login" class="nav-item nav-link">Login</router-link>
-            <!-- <button @click="logout" class="nav-item nav-link btn btn-link">Logout</button> -->
+            <router-link to="/register" class="nav-item nav-link">Register</router-link>
             <li class="nav-item"><button @click="logout" class="nav-link">Logout</button></li>
-
           </div>
         </div>
       </nav>
-
-      <div class="container">
-
-
-        <router-view></router-view>
-      </div>
+      <router-view></router-view>
     </div>
   </template>
 
-  <script setup>
-  import axios from 'axios';
-  import { useRouter } from 'vue-router';
-  const router = useRouter();
+<script setup>
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const logout=async()=> {
+let token=""
 
-  const logout = async () => {
-    let token = localStorage.getItem('token');
-    console.log(token);
+token=localStorage.getItem('token')
+console.log(token)
+axios.post('http://localhost:8000/api/logout', null, {
 
-    try {
-      await axios.post('http://localhost:8000/api/logout', null, {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      });
+headers: {
+'Authorization': `Bearer ${token}`
+}
+})
 
-      console.log(response);
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      router.push("/login");
-    } catch (err) {
-      console.error(err);
-      alert(err);
-    }
-  }
-  </script>
+.then((response) => {
+console.log(response)
+localStorage.removeItem('token')
+localStorage.removeItem('user')
+
+11
+router.push("/login")
+})
+.catch(err => {console.log(err);alert(err) })
+
+}
+</script>
+
